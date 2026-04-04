99395cc0075aaaf2f8dace70f7360a8b3a25bd76
hanhvn <vonhathanh@gmail.com>
2026-04-04 13:05:22 +0700

    add react router and basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Profile from "./components/Profile.jsx";
+import Book from "./components/Book.jsx";
+import Chapter from "./components/Chapter.jsx";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    Hello world!
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/chapter" element={<Chapter />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Chapter.jsx
@@ -0,0 +1,7 @@
+export default function Chapter() {
+  return (
+    <div>
+      <h1>Chapter</h1>
+    </div>
+  )
+}
